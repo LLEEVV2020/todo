e7433591f018d914f2390d28dbce999e2e58a168
lev <lleevv2020@gmail.com>
2023-10-12 19:21:38 +0400

    add111

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
+// https://platform.kata.academy/user/courses/3/1/3/7
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
