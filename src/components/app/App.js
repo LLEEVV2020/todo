6e92834ffe4560c9992cf7bf4435d71a70a195eb
lev <lleevv2020@gmail.com>
2023-10-13 20:26:52 +0400

    add

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -8,7 +8,7 @@ const Main = () => {
 
 function App() {
   const todoData = [
-    { id: 0, label: "Completed task", isCompleted: true },
+    { id: 0, label: "Completed task5", isCompleted: true },
     { id: 1, label: "Editing task2", isCompleted: false },
     { id: 2, label: "Active task3", isCompleted: true },
   ];
